b0db2534f75561b781a1b56cdc99c52a8a489b66
timothys-ekman <timsmith204@utexas.edu>
2018-11-06 17:03:07 -0600

    Only convert units if variable exists in dataset

--- tools/ds_utils.py
+++ tools/ds_utils.py
@@ -29,29 +29,35 @@ def convert_units(ds):
     # Convert datset units to something a little more intuitive
 
     # Precipitation from kg / m^3 * m/s to mm/day
-    ds['precipitation'].values = 86400 * ds['precipitation'].values
-    ds['precipitation'].attrs['units']='mm/day'
+    if 'precipitation' in ds.data_vars:
+        ds['precipitation'].values = 86400 * ds['precipitation'].values
+        ds['precipitation'].attrs['units']='mm/day'
     
     # Latent heat flux from W/m^2 to mm/day of evaporation
-    ds['flux_lhe'].values = 1/28 * ds['flux_lhe'].values
-    ds['flux_lhe'].attrs['units']='mm/day'
+    if 'flux_lhe' in ds.data_vars:
+        ds['flux_lhe'].values = 1/28 * ds['flux_lhe'].values
+        ds['flux_lhe'].attrs['units']='mm/day'
 
-    # bucket depth cond,conv,lh to mm/day
-    ds['bucket_depth_cond'].values = 86400 * 1000 * ds['bucket_depth_cond'].values
-    ds['bucket_depth_conv'].values = 86400 * 1000 * ds['bucket_depth_conv'].values
-    ds['bucket_depth_lh'].values = 86400 * 1000 * ds['bucket_depth_lh'].values
+    # bucket depth cond,conv,lh m/s to mm/day
+    if 'bucket_depth_cond' in ds.data_vars:
+        # Assume these all go together ...
+        ds['bucket_depth_cond'].values = 86400 * 1000 * ds['bucket_depth_cond'].values
+        ds['bucket_depth_conv'].values = 86400 * 1000 * ds['bucket_depth_conv'].values
+        ds['bucket_depth_lh'].values = 86400 * 1000 * ds['bucket_depth_lh'].values
 
-    ds['bucket_depth_cond'].attrs['units']='mm/day'
-    ds['bucket_depth_conv'].attrs['units']='mm/day'
-    ds['bucket_depth_lh'].attrs['units']='mm/day'
+        ds['bucket_depth_cond'].attrs['units']='mm/day'
+        ds['bucket_depth_conv'].attrs['units']='mm/day'
+        ds['bucket_depth_lh'].attrs['units']='mm/day'
 
     # Sea level pressure to kpa
-    ds['slp'].values = 1 / 1000 * ds['slp'].values
-    ds['slp'].attrs['units']='kPa'
+    if 'slp' in ds.data_vars:
+        ds['slp'].values = 1 / 1000 * ds['slp'].values
+        ds['slp'].attrs['units']='kPa'
 
     # Surface temperature to degC
-    ds['t_surf'].values = -273 + ds['t_surf'].values
-    ds['t_surf'].attrs['units']='degC'
+    if 't_surf' in ds.data_vars:
+        ds['t_surf'].values = -273 + ds['t_surf'].values
+        ds['t_surf'].attrs['units']='degC'
 
     return ds
 
