47378b763a5a834953b09bfb6a60b2c718d460de
Marianne Pietschnig <mp586@exeter.ac.uk>
2020-04-13 11:10:45 +0100

    correction to interplevs

--- interp_plevs_NLAND.py
+++ interp_plevs_NLAND.py
@@ -5,7 +5,6 @@ infiles = ['LandworldLakes_acdc_atmos_mean_5_to_50',    'Northland_dark_acdc_atm
 'Landworld_acdc_atmos_mean_5_to_50',         'Northland_dry_acdc_atmos_mean_5_to_50',
 'Northland_bright_acdc_atmos_mean_5_to_50',  'Northland_empty_acdc_atmos_mean_5_to_50']
 
-from isca.util import interpolate_output
 for infile_in in infiles:
     infile = '/scratch/mp586/Isca_DATA/Northland/'+infile_in+'.nc'
     outfile = '/scratch/mp586/Isca_DATA/Northland/'+infile_in+'_interp.nc'
